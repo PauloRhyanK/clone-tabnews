6e4890ec3753ff3d3ffc7a3ed075d044a5ee5959
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2024-12-10 20:14:11 +0000

    Adição do .nvmrc, package.json e instalação do Next, React e React-dom. Além da nosso primeira pagina.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+    <div>
+        <h2> Pra minha princesa que andou sozinha de onibus hoje:</h2>
+        <h1> Te amo! </h1>
+    </div>
+    )
+}
+
+export default Home;
