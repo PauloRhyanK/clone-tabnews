534220afad9bf6cf4a8868e1792bb8c75533f30d
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2024-12-12 14:25:14 +0000

    Atualização de mensagem v2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 function Home(){
     return (
     <div >
-        Teste
+        <h3>"Ainda que a figueira não floresça,
+             nem haja fruto na vide,
+             ainda que o produto da oliveria minta,
+             e nem mantimento nos campos,
+             ainda que o as ovelhas sejam arrebatadas do aprisco,
+             e nem hajam bois no curral,
+             Todavia eu me alegrarei no Senhor,
+             e me exultarei no Deus da minha salvação."
+            </h3>
+            <h6>Habacuque 3:17</h6>
     </div>
     )
 }
