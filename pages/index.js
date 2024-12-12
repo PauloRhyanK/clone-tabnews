3fed0f98ea85ed9f06bc39d849e3cdd7828ea3a2
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2024-12-12 14:10:00 +0000

    Delentando arquivos desnecessários

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 function Home(){
-    
-
     return (
-    <div class="div-main">
-        <h2> Pra minha princesa que andou sozinha de onibus ontem:</h2>
-        <h1> Te amo! </h1>
-        <h5>Estou com saudades...</h5>
-        <iframe src="https://giphy.com/embed/l2R0cE5EqO3QHiCoU" width="480" height="480" allowFullScreen></iframe><p></p>
+    <div >
+        Teste
     </div>
     )
 }
