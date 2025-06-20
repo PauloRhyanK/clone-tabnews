95ade8e5a9aeccbb63afd42d4bd121e15fa84ebd
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2025-06-20 02:16:34 +0000

    feat: status v1

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,22 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date();
+  const psVersionResult = await database.query("SELECT version();");
+  const psConn = await database.query("SELECT Count(*) as conn_in_use FROM pg_stat_activity;");
+  const psConnMax = await database.query("SELECT * FROM pg_settings WHERE name = 'max_connections';");
+  const psVersion = psVersionResult.rows[0].version;
+  const connMax = parseInt(psConnMax.rows[0].setting);
+  const connInUse = parseInt(psConn.rows[0].conn_in_use);
+  const connOpen = connMax - connInUse;
+  
   response.status(200).json({
    updated_at: updatedAt,  
+   database: {
+    database_version: psVersion,
+    conn_max: connMax,
+    conn_open: connOpen,
+    conn_in_use: connInUse,
+   },
   });
 }
 
--- tests/integra/pages/api/v1/status/get.test.js
+++ tests/integra/pages/api/v1/status/get.test.js
@@ -1,14 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  
+  const responseBody = await response.json();
+  console.log(responseBody)
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.database).toBeDefined();
+  expect(responseBody.database.database_version).toBeDefined();
+  expect(responseBody.database.conn_max).toBeDefined();
+  expect(responseBody.database.conn_in_use).toBeDefined();
+  expect(responseBody.database.conn_open).toBeDefined();
 
-  const reponseBody = await response.json();
-  expect(reponseBody.updated_at).toBeDefined();
-  expect(reponseBody.database).toBeDefined();
-  expect(reponseBody.database.database_version).toBeDefined();
-  expect(reponseBody.database.conn_max).toBeDefined();
-  expect(reponseBody.database.conn_in_use).toBeDefined();
-
-  const responseDate = new Date(reponseBody.updated_at).toISOString();
+  const responseDate = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(responseDate)
 });
